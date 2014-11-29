c59a1e1d7470e342b6a42863e7bceafde906c3d5
robinharms <robin@betahaus.net>
2014-11-29 14:38:43 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+TriArt.se README
+================
+
+Development version, see Arche project readme instead.
---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+requires = (
+    'Arche',
+    'pyramid',
+    'fanstatic',
+    'lingua',
+    'Babel',
+    )
+
+setup(name='voteit_site',
+      version='0.1dev',
+      description='voteit_site',
+      long_description=README,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='VoteIT development team',
+      author_email='robin@betahaus.net',
+      url='http://voteit.se',
+      keywords='web pylons pyramid voteit',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="voteit_site",
+      entry_points="""\
+      [fanstatic.libraries]
+      triart = triart_site.fanstatic_lib:triart_lib
+      """)
+#       message_extractors = { '.': [
+#               ('**.py',   'python', None ),
+#               ('**.pt',   'chameleon', None ),
+#               ]},
+#       )
