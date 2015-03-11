d6c71a3fc7f55d9ff4362e06867de3a11486f16f
robinharms <robin@betahaus.net>
2015-03-11 15:14:14 +0100

    First steps

--- voteit_site/__init__.py
+++ voteit_site/__init__.py
@@ -1,2 +1,7 @@
+
 def includeme(config):
-    pass
+    config.include('.fanstatic_lib')
+    config.override_asset(to_override='arche:templates/',
+                          override_with='voteit_site:templates/overrides/')
+    cache_max_age = int(config.registry.settings.get('arche.cache_max_age', 60*60*24))
+    config.add_static_view('voteit_site_static', 'voteit_site:static', cache_max_age = cache_max_age)
--- voteit_site/fanstatic_lib.py
+++ voteit_site/fanstatic_lib.py
@@ -7,9 +7,10 @@ from arche.fanstatic_lib import main_css
 
 
 voteit_site_lib = Library('voteit_site', 'static')
+voteit_site_css = Resource(voteit_site_lib, 'css/main.css', depends = (main_css,))
 
+def include_resources(view, event):
+    voteit_site_css.need()
 
-
-
-#def includeme(config):
-#    config.add_subscriber(something, [IBaseView, IViewInitializedEvent])
+def includeme(config):
+    config.add_subscriber(include_resources, [IBaseView, IViewInitializedEvent])
