38a34a58e0338a542b89e2d4208183f1ddb7b37b
robinharms <robin@betahaus.net>
2015-03-11 17:45:34 +0100

    Favicon

--- voteit_site/__init__.py
+++ voteit_site/__init__.py
@@ -5,3 +5,4 @@ def includeme(config):
                           override_with='voteit_site:templates/overrides/')
     cache_max_age = int(config.registry.settings.get('arche.cache_max_age', 60*60*24))
     config.add_static_view('voteit_site_static', 'voteit_site:static', cache_max_age = cache_max_age)
+    config.registry.settings['arche.favicon'] = 'voteit_site:static/favicon.ico'
